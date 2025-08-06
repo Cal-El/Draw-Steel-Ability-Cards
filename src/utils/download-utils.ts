a131e7c8c2a74003f78e9f2c4ef2d316dbb5fef9
Callum Grier <cal.grier@gmail.com>
2025-08-06 12:54:07 +1000

    Add carddata as metadata to download png and allow drag-and-drop import from png

--- src/utils/download-utils.ts
+++ src/utils/download-utils.ts
@@ -1,5 +1,7 @@
 import {toPng} from "html-to-image";
 import {Card, getCardTitle, getCardTopMatter} from "../types/card-list.ts";
+import {addMetadataFromBase64DataURI} from "meta-png"
+import {stringify as yamlStringify} from "yaml";
 
 function filizeString(s: string) {
     return s.replaceAll(' ', '-').toLowerCase();
@@ -17,7 +19,8 @@ export function saveImage(cardData: Card, cardId: string){
     if (!card) return
     toPng(card, {canvasWidth: 672, canvasHeight: 480, pixelRatio: 4, preferredFontFormat: "truetype"}, ).then(function (dataUrl) {
         const alink = document.createElement("a");
-        alink.href = dataUrl;
+        const metadataToAdd = encodeURI(yamlStringify(cardData, null, 2));
+        alink.href = addMetadataFromBase64DataURI(dataUrl, "cardData", metadataToAdd);
         alink.download = [getCardTopMatter(cardData), getCardTitle(cardData)].map(filizeString).join('_') + ".png";
         alink.click();
     })
