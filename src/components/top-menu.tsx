3600881de54c1bb0956b290541cb373a8c91deb6
Evelyne Grier <evelyne.rose@ymail.com>
2025-09-02 00:15:30 +1000

    fix: hide top menu when printing

--- src/components/top-menu.tsx
+++ src/components/top-menu.tsx
@@ -2,7 +2,7 @@ import { GiSwordman } from "react-icons/gi";
 
 export default function TopMenu({openHeroDataSidebar}: {openHeroDataSidebar: () => void}){
   return (
-    <div className="w-full bg-stone-400 text-stone-800 flex flex-row p-2 pl-4">
+    <div className="w-full bg-stone-400 text-stone-800 flex flex-row p-2 pl-4 print:hidden">
       <button onClick={openHeroDataSidebar} className="flex flex-row items-center text-base bg-stone-300 p-1 rounded-md hover:bg-stone-200 space-x-1"><GiSwordman/><span>Hero Data</span></button>
     </div>
   )
